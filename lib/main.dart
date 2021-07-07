dd2980d333dd9ce5f2428c196a5a972eaa130ed7
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-07-07 21:24:37 +0300

    extracted custom widget to file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:test_task/util/color/color_generator.dart';
 
+import 'custom.widgets/outlined_text_widget.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -29,46 +31,5 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
         onTap: changeColor);
-  }
-}
-
-class OutlinedText extends StatelessWidget {
-  final String text;
-  final double fontSize;
-  final double strokeWidth;
-  final Color textColor;
-  final Color outlineColor;
-
-  const OutlinedText({
-    Key? key,
-    this.text = "",
-    this.fontSize = 40,
-    this.strokeWidth = 4,
-    this.textColor = Colors.white,
-    this.outlineColor = Colors.black,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              fontSize: fontSize,
-              foreground: Paint()
-                ..style = PaintingStyle.stroke
-                ..strokeWidth = strokeWidth
-                ..color = outlineColor,
-            ),
-          ),
-        ),
-        Center(
-          child: Text(text,
-              style: TextStyle(fontSize: fontSize, color: textColor)),
-        ),
-      ],
-    );
   }
 }
